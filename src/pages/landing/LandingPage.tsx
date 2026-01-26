c7383793e6f7b50ca57091c067233ce5119a498a
adam-lev-barnett <adam.lev.barnett@gmail.com>
2026-01-26 16:37:41 -0600

    Bug error notification

--- src/pages/landing/LandingPage.tsx
+++ src/pages/landing/LandingPage.tsx
@@ -107,7 +107,7 @@ function LandingPage() {
         <main className="layout">
             <h1>Madlib Machine</h1>
             <section className="description" id="what">
-
+                <article className="update">I encountered my first bug! Thank you for your patience while I attempt to get the replacement word phase back up.</article>
                 <h2>What is the Madlib Machine?</h2>
                 <article className="explanation" id="app-summary">
                     <section className="simpleSummary">
